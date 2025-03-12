89eec990e38fbde20098c3d4087af1087a53d1bf
Hossein Taromi <hosseintaromi@Chainovas-Mac-mini.local>
2025-03-12 14:31:54 +0330

    refactor: :recycle: update git ignore and remove extra file

--- dist/domain/User/user.repository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserRepository = void 0;
-const user_entity_1 = require("./user.entity");
-const orm_1 = require("../../config/orm");
-exports.UserRepository = orm_1.AppDataSource.getRepository(user_entity_1.User);
